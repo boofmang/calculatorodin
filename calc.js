8590cf07583e8bd0ec4f4f9a03485be11a923d71
Lee Mcveagh <topmcveagh@gmail.com>
2025-10-16 13:08:05 +0100

    GITTTTTTTTTTTTT

---
+++ calc.js
@@ -0,0 +1,24 @@
+const add = function(num1, num2){
+    return num1 + num2;
+};
+
+const subtract = function(num1, num2){
+    return num1 - num2;
+}
+
+const divide = function(num1, num2){
+    return num1 / num2;
+}
+
+const multiply = function(num1, num2){
+    return num1 * num2;
+}
+
+
+let numberone = ``;
+let operator = ``;
+let numbertwo = ``;
+
+const operate = function(
+
+);
