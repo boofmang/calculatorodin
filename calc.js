8ddeb7db994625c691dc57af5b1eff6bf98aa940
Lee Mcveagh <topmcveagh@gmail.com>
2025-10-16 13:34:07 +0100

    Created operating function

--- calc.js
+++ calc.js
@@ -19,6 +19,21 @@ let numberone = ``;
 let operator = ``;
 let numbertwo = ``;
 
-const operate = function(
+const operate = function(numberone, operator, numbertwo){
+    numberone = Number(numberone);
+    numbertwo = Number(numbertwo);
 
-);
+    let result;
+    switch(operator){
+        case "+": result = add(num1, num2); break;
+        case "-": result = subtract(num1, num2); break;
+        case "x": result = multiply(num1, num2); break;
+        case "/": result = divide(num1, num2); break;
+        default: result = 0;
+    };
+
+
+
+};
+
+
