5ecfc467b55fba28978ad87b5ac629f79f9ee499
Lee Mcveagh <topmcveagh@gmail.com>
2025-10-22 17:13:25 +0100

    added functionality to the calculator - still needs fixing

--- calc.js
+++ calc.js
@@ -1,22 +1,69 @@
-
+/////global////////
 let numberone = "";
 let operator = "";
 let numbertwo = "";
 let display = document.getElementById("displaytext");
 let currentnumber = "";
-const numberbuttons = document.querySelectorAll(".numbers")
+const numberbuttons = document.querySelectorAll(".numbers");
+const operatorbuttons = document.querySelectorAll(".operators");
+const clearall = document.querySelector(".clearall");
+const back = document.querySelector(".delete");
+const evaluate = document.querySelector(".equals");
 
-
+/////butttons///////
 numberbuttons.forEach(button => {
     const value = button.textContent;
     const newvalue = parseFloat(value);
     button.addEventListener("click", () => updateNumber(newvalue));   
 });
 
+clearall.addEventListener("click", () => {
+    display.textContent = "";
+    currentnumber = "";
+    numberone = "";
+    numbertwo = "";
+    operator = "";
+})
+
+back.addEventListener("click", () => {
+    display.textContent = display.textContent.slice(0, -1);
+})
+
 function updateNumber(number) {
-    display.textContent += parseFloat(number);
+
+    display.textContent += String(number);
+    currentnumber = display.textContent;
 }
 
+
+operatorbuttons.forEach(button => {
+    const value = button.textContent;
+    button.addEventListener("click", () => {
+        operator = value;
+        numberone = currentnumber;
+        display.textContent = "";
+    })
+})
+
+evaluate.addEventListener("click", () => {
+    const running = display.textContent;
+    numbertwo = running;
+    display.textContent = "";
+    if(operator === "/" && numbertwo === "0") {
+        display.textContent = "NOPE"
+        return;
+    }
+    else{
+    const result = operate(numberone, operator, numbertwo);
+    // store and display the result as a string; also use it as the next left operand
+    currentnumber = String(result);
+    numberone = currentnumber;
+    display.textContent = currentnumber;
+    }
+});
+
+
+////mathetmatical operators////////
 const add = function(num1, num2){
     return num1 + num2;
 };
@@ -33,7 +80,7 @@ const multiply = function(num1, num2){
     return num1 * num2;
 }
 
-
+//////Calculation function/////////
 const operate = function(numberone, operator, numbertwo){
     numberone = parseFloat(numberone);
     numbertwo = parseFloat(numbertwo);
